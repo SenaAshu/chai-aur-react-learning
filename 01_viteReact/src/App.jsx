1b8b1e2a4d2ea98e18fa08d10176773a14b3377d
SenaAshu <ashutoshsenapati098@gmail.com>
2024-12-24 22:26:26 +0530

    1st dAY

---
+++ 01_viteReact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>Chai Aur React || Ashutosh Senapati</h1>
+  )
+}
+
+export default App
--- 01vitereact/src/chai.jsx
+++
@@ -1,6 +0,0 @@
-function Maxi(){
-    return(
-        <h3>Hello there!</h3>
-    )
-}
-export default Maxi
